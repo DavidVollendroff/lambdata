6d507a0274a361c2d8ec09a11b576a676da305bb
David Vollendroff <civilianemail@gmail.com>
2020-01-13 10:35:44 -0800

    added utility files

---
+++ lambdata_davidvollendroff/__init__.py
@@ -0,0 +1,31 @@
+"""
+Collection of Data Science helper functions
+"""
+
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+
+def confusion_plot(y_true, y_pred, cmap='viridis'):
+  """
+  Plots a confusion matrix using the Seaborn library
+  cmap: Color-mapping
+  :param y_true:
+  :param y_pred:
+  :param cmap:
+  :return:
+  """
+  labels = unique_labels(y_val)
+  columns = [f'Predicted {label}' for label in labels]
+  index = [f'Actual {label}' for label in labels]
+  table = pd.DataFrame(confusion_matrix(y_true, y_pred),
+                       columns=columns,
+                       index=index)
+  return sns.heatmap(table, annot=True, fmt='d', cmap=cmap)
+
+ONES = pd.DataFrame(np.ones(10))
+
+ZEROS = pd.DataFrame(np.zeros(50))
---
+++ lambdata_davidvollendroff/df_utils.py
@@ -0,0 +1,7 @@
+"""
+Utility functions for working with DataFrames
+"""
+
+import pandas
+
+TEST_DF = pandas.DataFrame([1,2,3])
